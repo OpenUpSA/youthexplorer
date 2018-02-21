af319debe036851498e6eb7589baa2a4a03b4a8f
Damian <damian@code4sa.org>
2018-02-21 16:33:59 +0400

    Update release year

--- wazimap_za/profiles/youth.py
+++ wazimap_za/profiles/youth.py
@@ -903,7 +903,7 @@ def get_health_profile(geo, session, display_profile, comparative=False):
 
     if display_profile == 'WC' and geo.geo_level != 'ward':
         # We don't have data on ward level for the following
-        with dataset_context(year='2016'):
+        with dataset_context(year='2017'):
             youth_pregnancy_rate_by_year, _ = get_stat_data(
                 ['year'], geo, session,
                 table_universe='Teenage pregnancy rate by year',
