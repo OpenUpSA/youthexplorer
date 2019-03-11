0863a9b6e1806850e5491a9cbd1bb34461dbe0ce
lunga <stumenz.complex@gmail.com>
2019-03-11 14:13:56 +0200

    add urls for explorer api

---
+++ wazimap_za/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^', include('wazimap.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^explorer/', include('explorer.urls'))
+]
