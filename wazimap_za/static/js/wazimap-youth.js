b785a67257c3fa2ac00f040da9d8ec1d70bf4cd1
lunga <stumenz.complex@gmail.com>
2019-04-05 12:04:42 +0200

    add banner for YE to link to google form

---
+++ wazimap_za/static/js/wazimap-youth.js
@@ -0,0 +1,5 @@
+$(function() {
+  // add announcement banner
+  var banner = $('<div id="banner"><div class="wrapper">Please register <a href="https://docs.google.com/forms/d/e/1FAIpQLSc3szkGe77kgZaFn-DvrUnl8z6roV2iz1dJ8C--gLewGjvF8A/viewform">here</a> to receive notifications of data updates and other important news</div></div>');
+  $('#page-header').append(banner);
+});
