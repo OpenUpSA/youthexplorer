d92e8875a3b7a643d66756057e3d18bcb6ef942c
lunga <stumenz.complex@gmail.com>
2019-07-31 14:16:20 +0200

    namespace explorer

--- wazimap_za/urls.py
+++ wazimap_za/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('wazimap.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^explorer/', include('explorer.urls'))
+    url(r"^", include("wazimap.urls")),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^explorer/", include("explorer.urls", namespace="explorer")),
 ]
