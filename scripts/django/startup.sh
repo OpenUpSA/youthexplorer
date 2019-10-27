d7af2a3a42d85040fe748a34e4b069684075043d
Adi Eyal <adi@openup.org.za>
2019-10-27 21:46:06 +0200

    Added 4 threads per worker to gunicorn

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -1,3 +1,3 @@
 python2 manage.py collectstatic --noinput
 python2 manage.py migrate
-exec gunicorn wazimap_za.wsgi -b 0.0.0.0:8000 --access-logfile geo.access.log --reload --error-logfile geo.error.log --worker-class=gevent
+exec gunicorn wazimap_za.wsgi -b 0.0.0.0:8000 --access-logfile geo.access.log --reload --error-logfile geo.error.log --worker-class=gevent --threads=4
