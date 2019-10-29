0538a928d9bad044f93806889254db631d17b3dc
Adi Eyal <adi@openup.org.za>
2019-10-29 17:13:08 +0200

    Added conditionals on the startup script
    Don't want to clobber the database everytime

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-sleep 3
-/app/scripts/django/wait-for-postgres.sh $DATABASE_HOST
+if [ "$YE_WAIT_FOR_DATABASE" = "True" ]
+then
+    sleep 3
+    /app/scripts/django/wait-for-postgres.sh $DATABASE_HOST
+fi
 
-echo "Collecting static files"
-python2 manage.py collectstatic --noinput >> /tmp/startup.log 2>> /tmp/startup.err
+if [ "$YE_LOAD_DATA" = "True" ]
+then
+    echo "Collecting static files"
+    python2 manage.py collectstatic --noinput >> /tmp/startup.log 2>> /tmp/startup.err
 
-echo "Migrating database"
-python2 manage.py migrate >> /tmp/startup.log 2>> /tmp/startup.err
+    echo "Migrating database"
+    python2 manage.py migrate >> /tmp/startup.log 2>> /tmp/startup.err
 
-echo "Loading fixtures"
-echo "Loading data files"
-cat /app/sql/*.sql | python manage.py dbshell >> /tmp/startup.log 2>> /tmp/startup.err
+    echo "Loading fixtures"
+    echo "Loading data files"
+    cat /app/sql/*.sql | python manage.py dbshell >> /tmp/startup.log 2>> /tmp/startup.err
 
-echo "Loading boundary data"
-python manage.py loaddata provinces.json.gz >> /tmp/statup.log 2>> /tmp/startup.err
+    echo "Loading boundary data"
+    python manage.py loaddata provinces.json.gz >> /tmp/statup.log 2>> /tmp/startup.err
 
-echo "Loading point data"
-python manage.py loaddata fixtures/explorer.json.gz >> /tmp/startup.loga 2>> /tmp/startup.err
+    echo "Loading point data"
+    python manage.py loaddata fixtures/explorer.json.gz >> /tmp/startup.loga 2>> /tmp/startup.err
 
-echo "Loading wazimap geo data"
-python manage.py loaddata fixtures/wazimap_django_models.json.gz >> /tmp/startup.log 2>> /tmp/startup.err
+    echo "Loading wazimap geo data"
+    python manage.py loaddata fixtures/wazimap_django_models.json.gz >> /tmp/startup.log 2>> /tmp/startup.err
+fi
 
 echo "Starting gunicorn"
 exec gunicorn youthexplorer.wsgi -b 0.0.0.0:5000 --access-logfile geo.access.log --reload --error-logfile geo.error.log --worker-class=gevent --threads=4
